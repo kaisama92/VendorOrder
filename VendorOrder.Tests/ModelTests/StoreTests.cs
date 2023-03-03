f4c8f90478f2276571fb6c9ed53f2b74205a2d9d
Kai Clausen <kaiclausen123@gmail.com>
2023-03-03 09:59:11 -0800

    update store.cs and storetests.cs for less functionality

--- VendorOrder.Tests/ModelTests/StoreTests.cs
+++ VendorOrder.Tests/ModelTests/StoreTests.cs
@@ -16,39 +16,8 @@ namespace VendorOrder.Tests
     [TestMethod]
     public void StoreConstructor_CreatesInstanceOfStore_Store()
     {
-      string name = "Pierre's";
-      Store newStore = new Store(name);
+      Store newStore = new Store();
       Assert.AreEqual(typeof(Store), newStore.GetType());
     }
-
-    [TestMethod]
-    public void GetStoreName_ReturnsStoreName_String()
-    {
-      string name = "Pierre's";
-      Store pierre = new Store(name);
-      string result = pierre.Name;
-      Assert.AreEqual(name, result);
-    }
-
-    [TestMethod]
-    public void SetStoreName_SetsStoreName_String()
-    {
-      string name = "Pierre's";
-      string updatedName = "John's";
-      Store pierre = new Store(name);
-      pierre.Name = updatedName;
-      string result = pierre.Name;
-      Assert.AreEqual(updatedName, result);
-    }
-
-    [TestMethod]
-    public void GetId_ReturnsStoreId_Int()
-    {
-      string name = "Pierre's";
-      Store pierre = new Store(name);
-      int result = pierre.Id;
-      Assert.AreEqual(1, result);
-    }
-
   }
 }
--- VendorOrder/Models/Store.cs
+++ VendorOrder/Models/Store.cs
@@ -5,20 +5,12 @@ namespace VendorOrder.Models
 {
   public class Store
   {
-    private static List<Store> _storeInstances = new List<Store> {};
     public int Id { get;}
     public string Name { get; set; }
 
-    public Store(string name)
-    {
-      Name = name;
-      _storeInstances.Add(this);
-      Id = _storeInstances.Count;
-    }
-
     public static void ClearAll()
     {
-      _storeInstances.Clear();
+      _instances.Clear();
     }
   }
 }
